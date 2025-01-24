9c8b97a17dfe669563d338b685db458e37d3ef41
Cristina Ruiz <cristina.ruiz@iesdoctorbalmis.com>
2025-01-24 12:55:49 +0100

    Agregar componente de artículos y ruta correspondiente en el enrutador

--- Save&Serve/src/app/app-routing.module.ts
+++ Save&Serve/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './pages/index/index.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticlesComponent } from './pages/articles/articles.component';
 
 const routes: Routes = [
-  { path: '', component: IndexComponent }, 
+  { path: '', component: IndexComponent },
+  {path: 'articles', component: ArticlesComponent}, 
  
 ];
 
---
+++ Save&Serve/src/app/pages/banco-alimentos/banco-alimentos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banco-alimentos',
+  imports: [],
+  templateUrl: './banco-alimentos.component.html',
+  styleUrl: './banco-alimentos.component.scss'
+})
+export class BancoAlimentosComponent {
+
+}
