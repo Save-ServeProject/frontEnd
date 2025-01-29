68c198d7c5a245a71a57a1400e1ebf9759268bdb
Cristina Ruiz <cristina.ruiz@iesdoctorbalmis.com>
2025-01-29 11:51:04 +0100

    Agregar componente de pasarela de pago con plantilla y pruebas unitarias

---
+++ Save&Serve/src/app/pages/pasarela-pago/pasarela-pago.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pasarela-pago',
+  imports: [],
+  templateUrl: './pasarela-pago.component.html',
+  styleUrl: './pasarela-pago.component.scss'
+})
+export class PasarelaPagoComponent {
+
+}
