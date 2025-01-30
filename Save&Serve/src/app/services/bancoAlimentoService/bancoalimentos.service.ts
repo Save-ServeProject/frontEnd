b702c90338c65bd4946848bb9c87db53c2c445eb
Leticia-FR <leticia.fernandez@iesdoctorbalmis.com>
2025-01-30 12:21:41 +0100

    Agregar métodos para obtener, crear, actualizar y eliminar bancos de alimentos en BancoalimentosService

--- Save&Serve/src/app/services/bancoAlimentoService/bancoalimentos.service.ts
+++ Save&Serve/src/app/services/bancoAlimentoService/bancoalimentos.service.ts
@@ -1,14 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { BancoDeAlimentos } from '../../models/bancoAlimentos.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BancoalimentosService {
-  private url = 'http://localhost:9000/bancos';
+  private apiUrl = 'http://localhost:9000/bancos';
 
   constructor(private http: HttpClient) { }
 
- 
+  getAll(): Observable<BancoDeAlimentos[]> {
+    return this.http.get<BancoDeAlimentos[]>(this.apiUrl);
+  }
+
+  getById(id: number): Observable<BancoDeAlimentos> {
+    return this.http.get<BancoDeAlimentos>(`${this.apiUrl}/${id}`);
+  }
+
+  create(banco: BancoDeAlimentos): Observable<BancoDeAlimentos> {
+    return this.http.post<BancoDeAlimentos>(this.apiUrl, banco);
+  }
+
+  update(id: number, banco: BancoDeAlimentos): Observable<BancoDeAlimentos> {
+    return this.http.put<BancoDeAlimentos>(`${this.apiUrl}/${id}`, banco);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
+
+
