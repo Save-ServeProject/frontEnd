7848d8c9dc017225f32aeae9807a44ec57bdc4e0
Cristina Ruiz <cristina.ruiz@iesdoctorbalmis.com>
2025-01-30 12:05:49 +0100

    Corregir la ruta de importación del modelo Alergenos en el servicio de alérgenos

--- Save&Serve/src/app/services/alergenosService/alergenos.service.ts
+++ Save&Serve/src/app/services/alergenosService/alergenos.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Alergenos } from '../alergenosService/';
+import { Alergenos } from '../../models/alergenos.model';
 
 @Injectable({
   providedIn: 'root'
