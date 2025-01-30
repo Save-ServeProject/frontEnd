2fc33f2619a623c5a5015dd71cf1146911e6a866
Leticia-FR <leticia.fernandez@iesdoctorbalmis.com>
2025-01-30 12:04:31 +0100

    Agregar servicio Alergenos con métodos para obtener, crear, actualizar y eliminar alérgenos

--- Save&Serve/src/app/services/alergenosService/alergenos.service.ts
+++ Save&Serve/src/app/services/alergenosService/alergenos.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Alergenos } from '../alergenosService/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlergenosService {
+  private apiUrl = 'http://localhost:8080/api/alergenos';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Alergenos[]> {
+    return this.http.get<Alergenos[]>(this.apiUrl);
+  }
+
+  getById(id: number): Observable<Alergenos> {
+    return this.http.get<Alergenos>(`${this.apiUrl}/${id}`);
+  }
+
+  create(alergeno: Alergenos): Observable<Alergenos> {
+    return this.http.post<Alergenos>(this.apiUrl, alergeno);
+  }
+
+  update(id: number, alergeno: Alergenos): Observable<Alergenos> {
+    return this.http.put<Alergenos>(`${this.apiUrl}/${id}`, alergeno);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
