fa0551feb8cc7129498b0163edb2e27342046a30
Leticia-FR <leticia.fernandez@iesdoctorbalmis.com>
2025-01-30 11:36:08 +0100

    Agregar interfaz Empresa con propiedades básicas

---
+++ Save&Serve/src/app/models/empresa.model.ts
@@ -0,0 +1,14 @@
+export interface Empresa {
+    id?: number;
+    nombre: string;
+    email: string;
+    direccion?: string;
+    telefono?: string;
+    cif: string;
+    contrasenia: string;
+    tipo?: string;
+    ciudad?: string;
+    suscripcion?: 'ADMIN' | 'USER' | 'GUEST';
+   // donaciones?: Donacion[];
+    //Puede que toque cambiar cosas cuando la base de datos este
+}
