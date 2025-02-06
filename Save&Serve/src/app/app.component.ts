43a977446115d232869dc285a797c87fdcac0234
Cristina Ruiz <cristina.ruiz@iesdoctorbalmis.com>
2025-02-06 12:40:58 +0100

    Agregar componente Scrolltop: implementar botón para volver al inicio de la página y su estilo

--- Save&Serve/src/app/app.component.ts
+++ Save&Serve/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar-component/navbar-component.component';
 import { FooterComponent } from './components/footer-component/footer-component.component';
 import { ArticuloService } from './services/articuloService/articulo.service';
+import { ScrolltopComponent } from './components/scrolltop/scrolltop.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule,NavbarComponent,FooterComponent],
+  imports: [CommonModule, RouterModule,NavbarComponent,FooterComponent, ScrolltopComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
